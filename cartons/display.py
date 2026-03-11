fa25f556d29c0df83998d4d848e9c6ede0712c2d
AndPan3 <andrejbajusic2012@gmail.com>
2026-03-11 22:15:28 +0100

    update display.py error kept showing added "coordinates" line 7 and instead of Polyline changed to PolyLine on line 15

--- cartons/display.py
+++ cartons/display.py
@@ -1,15 +1,21 @@
 from .routing import get_route
 import folium
-def draw(base_url, lon1, lat1, lon2, lat2, col, weight):
 
+def draw(base_url, lon1, lat1, lon2, lat2, col="blue", weight=5):
     route = get_route(base_url, lon1, lat1, lon2, lat2)
 
-    routecoords = route.geometry #shame commit..
+    routecoords = route.geometry["coordinates"]
     foliumcoords = [[lat, lon] for lon, lat in routecoords]
+
     m = folium.Map(
-    location=(lat1, lon1),
-    control_scale=True,
+        location=(lat1, lon1),
+        control_scale=True,
     )
-    folium.Polyline(
-        foliumcoords, color=col, weight=weight).add_to(m)
+
+    folium.PolyLine(
+        foliumcoords,
+        color=col,
+        weight=weight
+    ).add_to(m)
+
     return m
