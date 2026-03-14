003f556863625690c9dce08afb0f319f75600fe1
Andrej Bajusic <andrejbajusic2012@gmail.com>
2026-03-14 09:34:40 +0100

    Update display.py

--- cartons/display.py
+++ cartons/display.py
@@ -2,8 +2,7 @@ from .routing import get_route
 import folium
 
 def draw(base_url, lon1, lat1, lon2, lat2, col="blue", weight=5,tiles="OpenStreetMap", transport="car"):
-    route = get_route(base_url, lon1, lat1, lon2, lat2)
-
+    route = get_route(base_url, lon1, lat1, lon2, lat2, transport)
     routecoords = route.geometry
     foliumcoords = [[lat, lon] for lon, lat in routecoords]
 
