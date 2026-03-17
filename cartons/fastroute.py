84ac0e1d99b265d799d313dcfed7741a839fc647
AndPan3 <andrejbajusic2012@gmail.com>
2026-03-17 22:04:30 +0100

    made fastroute, mabye rename?, rough sketch, will work, will polish it tomorrow. required are Base_url, lon1,lat1,lon2,lat2,transport, transport needs to be str

---
+++ cartons/fastroute.py
@@ -0,0 +1,24 @@
+import folium
+from .routing import get_route
+def fastroute(
+        base_url,lon1,lat1,lon2,lat2,transport
+):
+    route=get_route(base_url,lon1,lat1,lon2,lat2,transport)
+
+    routecoords = route.geometry
+    fastroutecoords = [[lat, lon] for lon, lat in routecoords]
+
+    m = folium.Map(
+        tiles="CartoDB Positron",
+        attr="Copyright: CartoDB Positron",
+        control_scale=True,
+
+    )
+
+    folium.PolyLine(
+        fastroutecoords,
+        color="red",
+        weight=5
+    ).add_to(m)
+    m.fit_bounds(fastroutecoords)
+    return m
