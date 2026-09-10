d7b5aa7806517111151c7edec4a62306b5b356e4
AndPan3 <andrejbajusic2012@gmail.com>
2026-09-10 20:56:26 +0200

    Enhance map_route calls with additional parameters for color, weight, tiles, and attribution

--- otherfiles/Testing/map_route.py
+++ otherfiles/Testing/map_route.py
@@ -1,3 +1,19 @@
 from cartons import map_route
-map_route(base_url="http://router.project-osrm.org", coords=[(13.388860, 52.517037), (13.397634, 52.529407)], osrm_profile="driving")
-map_route(base_url="http://router.project-osrm.org", coords=[(13.388860, 52.517037), (47.349375, 7.907760),(13.397634, 52.529407)], osrm_profile="driving")
+map_route(
+    base_url="http://router.project-osrm.org",
+    coords=[(13.388860, 52.517037), (13.397634, 52.529407)],
+    osrm_profile="driving",
+    color="red",
+    weight=5,
+    tiles="OpenStreetMap",
+    attribution="OpenStreetMap contributors",
+)
+map_route(
+    base_url="http://router.project-osrm.org",
+    coords=[(13.388860, 52.517037), (47.349375, 7.907760),(13.397634, 52.529407)],
+    osrm_profile="driving",
+    color="blue",
+    weight=10,
+    tiles="CartoDB Dark_Matter",
+    attribution="© CartoDB Dark_Matter"
+)
